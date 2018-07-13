6de028d15e6946d5e7db90f3ee5e867225ce06ad
James Wilson <wilsjame@oregonstate.edu>
2018-07-12 21:54:12 -0700

    add crawl settings form
    TODO:  add BF page limit, add DF depth limit, styling..

--- web/home.go
+++ web/home.go
@@ -5,9 +5,31 @@
 package main
 
 import (
+	"html/template"
+	"net/http"
 	"fmt"
 )
 
+type FormPage struct{
+	URL string
+	Type string
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	p := FormPage{URL: "starting.url", Type: "Depth-first"} // Hardcoded values
+	t, _ := template.ParseFiles("basictemplating.html")
+	t.Execute(w, p)
+
+}
+
+
+
+func indexhandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hey, %s</h1>", "<strong>there</strong>")
+}
+
 func main() {
-	fmt.Printf("Home\n")
+	http.HandleFunc("/", indexhandler)
+	http.HandleFunc("/form/", formHandler)
+	http.ListenAndServe(":80", nil)
 }
