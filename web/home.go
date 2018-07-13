b4bf5bb22e80f2b6f1e8e49f3126ac37b5fe3b98
James Wilson <wilsjame@oregonstate.edu>
2018-07-13 15:14:58 -0700

    debug print struct names+values
    Only printed values before.

--- web/home.go
+++ web/home.go
@@ -39,7 +39,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		BL: r.FormValue("BL"),
 		DL: r.FormValue("DL"),
 	}
-	fmt.Println(crawl) // debug
+	fmt.Printf("%+v\n", crawl) // debug
 
 	// TODO format crawl settings as needed
 	_ = crawl
